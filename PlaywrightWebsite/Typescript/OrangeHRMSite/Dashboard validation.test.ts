7b036d51a3b4952a8bbeb34884b4204fcda0947b
Ramnath <ramchinna058@gmail.com>
2024-12-30 03:00:42 +0530

    commit

---
+++ PlaywrightWebsite/Typescript/OrangeHRMSite/Dashboard validation.test.ts
@@ -0,0 +1,41 @@
+import {test,expect} from '@playwright/test';
+
+test.beforeEach(async({page}) =>{
+
+    try {
+        await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login', { waitUntil: 'load' });
+    } catch (error) {
+        console.error('Error navigating:', error);
+    };
+    
+});
+test.describe('Validating Dashboard', () =>{
+    test('Validating Dashboard side ribbons', async({page}) => {
+        
+        let username: string = 'Admin';
+        let passoword: string = 'admin123';
+        //creating Elements 
+        const HRMLogin = page.getByRole('img', {name:'company-branding'});
+        const Login_header = page.getByRole('heading', {name:'Login'});
+        const HRM_input = page.getByPlaceholder('Username');
+        const HRM_Password = page.getByPlaceholder('Password');
+        const HRM_LoginButton = page.getByRole('button', {name:'Login'});
+        const HRM_forgotPassword = page.getByText('Forgot your Password?');
+        const HRM_logo = page.getByAltText('orangehrm-logo').first();
+        const HRM_Dashboard = page.getByRole('heading',{name:'Dashboard'});
+        // Idetifying Created Elements
+        await expect(HRMLogin).toBeVisible();
+        await expect(Login_header).toHaveText('Login');
+        await HRM_input.fill(username);
+        await HRM_Password.fill(passoword);
+        await expect(HRM_LoginButton).toBeEnabled();
+        await expect(HRM_forgotPassword).toBeVisible();
+        await expect(HRM_logo).toBeHidden();
+        await HRM_LoginButton.click();
+        let URL = page.url();
+        console.log(URL);
+        await expect(HRM_Dashboard).toHaveRole('heading');
+       
+        console.log('Validating Dashboard items');
+    })
+})
